5522b5e0e57b7fba1e3095c24b5bb9a671737bf9
jihyun37 <106298280+shin-sos@users.noreply.github.com>
2022-07-13 11:14:11 +0900

    upyeah

---
+++ java/nslide.js
@@ -0,0 +1,20 @@
+$(function(){
+
+  $(".slide").slick({
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
+  
+  $(".slide1").slick({
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
+
+  $(".slide2").slick({
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
+});
